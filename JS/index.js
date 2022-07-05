1383162980118fe60a881cdf92b9dbf2aa3457e6
= <ismoilbek20050421@gmail.com>
2022-07-05 10:17:23 +0500

    Castaway

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,8 @@
 // CASTAWAY WEB-SITE TO THE CODES
+
+window.addEventListener("load",()=>{
+  window.addEventListener("scroll",()=>{
+    let header = document.querySelector(".header");
+    header.classList.toggle("active",window.scrollY > 0);
+  });
+});
