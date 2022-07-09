c91993e620f744a2e4978c38c55b9c4c54db92f2
= <ismoilbek20050421@gmail.com>
2022-07-09 17:39:55 +0500

    Castaway

--- JS/index.js
+++ JS/index.js
@@ -5,5 +5,23 @@ window.addEventListener("load", () => {
     let header = document.querySelector(".header");
     header.classList.toggle("active", window.scrollY > 0);
   });
-  AOS.init()
+  AOS.init();
+
+  let timesBtn = document.querySelector(".fa-times");
+  let barsBtn = document.querySelector(".fa-bars");
+  let navbar = document.querySelector(".secret__navbar");
+  let linkBtn = document.querySelectorAll(".secret__navbar__link");
+  barsBtn.addEventListener("click",()=>{
+    navbar.classList.add("active");
+  });
+
+  timesBtn.addEventListener("click",()=>{
+    navbar.classList.remove("active");
+  });
+
+  linkBtn.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      navbar.classList.remove("active");
+    });
+  });
 });
