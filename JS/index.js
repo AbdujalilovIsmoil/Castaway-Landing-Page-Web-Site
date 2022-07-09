bdee03d185eb286470e3e7e2c94e4760c015651b
= <ismoilbek20050421@gmail.com>
2022-07-09 19:18:54 +0500

    Castaway

--- JS/index.js
+++ JS/index.js
@@ -5,22 +5,25 @@ window.addEventListener("load", () => {
     let header = document.querySelector(".header");
     header.classList.toggle("active", window.scrollY > 0);
   });
+
+  // AOS JAVASCRIPT LIBRARY
   AOS.init();
 
+  // SECRET NAVBAR JAVASCRIPT TO THE CODES
   let timesBtn = document.querySelector(".fa-times");
   let barsBtn = document.querySelector(".fa-bars");
   let navbar = document.querySelector(".secret__navbar");
   let linkBtn = document.querySelectorAll(".secret__navbar__link");
-  barsBtn.addEventListener("click",()=>{
+  barsBtn.addEventListener("click", () => {
     navbar.classList.add("active");
   });
 
-  timesBtn.addEventListener("click",()=>{
+  timesBtn.addEventListener("click", () => {
     navbar.classList.remove("active");
   });
 
-  linkBtn.forEach((item)=>{
-    item.addEventListener("click",()=>{
+  linkBtn.forEach((item) => {
+    item.addEventListener("click", () => {
       navbar.classList.remove("active");
     });
   });
