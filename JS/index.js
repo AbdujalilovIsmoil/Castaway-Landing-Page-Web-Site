57154678900e35165d9da95d06444b3ac18ef605
= <ismoilbek20050421@gmail.com>
2022-12-20 12:33:26 +0500

    Castaway Web-Site Responsive The End

--- JS/index.js
+++ JS/index.js
@@ -2,29 +2,24 @@
 
 window.addEventListener("load", () => {
   window.addEventListener("scroll", () => {
-    let header = document.querySelector(".header");
-    header.classList.toggle("active", window.scrollY > 0);
+    $(".header").classList.toggle("active", window.scrollY > 0);
   });
 
   // AOS JAVASCRIPT LIBRARY
   AOS.init();
 
   // SECRET NAVBAR JAVASCRIPT TO THE CODES
-  let timesBtn = document.querySelector(".fa-times");
-  let barsBtn = document.querySelector(".fa-bars");
-  let navbar = document.querySelector(".secret__navbar");
-  let linkBtn = document.querySelectorAll(".secret__navbar__link");
-  barsBtn.addEventListener("click", () => {
-    navbar.classList.add("active");
+  $(".fa-bars").addEventListener("click", () => {
+    $(".secret__navbar").classList.add("active");
   });
 
-  timesBtn.addEventListener("click", () => {
-    navbar.classList.remove("active");
+  $(".fa-times").addEventListener("click", () => {
+    $(".secret__navbar").classList.remove("active");
   });
 
-  linkBtn.forEach((item) => {
+  $$(".secret__navbar__link").forEach((item) => {
     item.addEventListener("click", () => {
-      navbar.classList.remove("active");
+      $(".secret__navbar").classList.remove("active");
     });
   });
 });
